79d8cda72a7933d4fe05382a41a8cebb6a2c5559
Agustin Ariel <agustin.ariel31@gmail.com>
2020-12-07 17:16:38 -0300

    updateUserDetails ... created and controller succesfully built

--- src/main/java/com/RestFulWebService/demo/controller/UserController.java
+++ src/main/java/com/RestFulWebService/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.RestFulWebService.demo.controller;
 
+import com.RestFulWebService.demo.model.request.UpdateUserDetailsRequestModel;
 import com.RestFulWebService.demo.model.request.UserDetailsRequestModel;
 import com.RestFulWebService.demo.model.response.UserRest;
 import org.springframework.http.HttpStatus;
@@ -7,10 +8,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
+
+    Map<String, UserRest> users;
 
     @GetMapping
     public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page, @RequestParam(value = "limit", defaultValue = "50") int limit, @RequestParam(value = "sort", required = false) String sort) {
@@ -19,13 +25,10 @@ public class UserController {
 
     @GetMapping(path = "/{userId}")
     public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
-        UserRest returnValue = new UserRest();
-
-        returnValue.setEmail("test@test.com");
-        returnValue.setFirstName("Gomito");
-        returnValue.setLastName("Gomito");
-
-        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
+        if (users.containsKey(userId)) {
+            return new ResponseEntity<>(users.get(userId), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping
@@ -36,17 +39,31 @@ public class UserController {
         returnValue.setFirstName(userDetails.getFirstName());
         returnValue.setLastName(userDetails.getLastName());
 
+        returnValue.setUserId(UUID.randomUUID().toString());
+
+        if (users == null)
+            users = new HashMap<>();
+
+        users.put(returnValue.getUserId(), returnValue);
+
         return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }
 
-    @PutMapping
-    public String updateUser() {
-        return "update user was called";
+    @PutMapping(path = "/{userId}")
+    public UserRest updateUser(@PathVariable String userId, @Valid @RequestBody UpdateUserDetailsRequestModel userDetails) {
+        UserRest storedUserDetails = users.get(userId);
+        storedUserDetails.setFirstName(userDetails.getFirstName());
+        storedUserDetails.setLastName(userDetails.getLastName());
+
+        users.put(userId, storedUserDetails);
+
+        return storedUserDetails;
     }
 
-    @DeleteMapping
-    public String deleteUser() {
-        return "delete user was called";
+    @DeleteMapping(path = "/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+        users.remove(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/RestFulWebService/demo/model/request/UpdateUserDetailsRequestModel.java
+++ src/main/java/com/RestFulWebService/demo/model/request/UpdateUserDetailsRequestModel.java
@@ -2,7 +2,6 @@ package com.RestFulWebService.demo.model.request;
 
 import lombok.*;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,22 +10,13 @@ import javax.validation.constraints.Size;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDetailsRequestModel {
+public class UpdateUserDetailsRequestModel {
 
     @NotNull(message = "First Name cannot be null")
     @Size(min = 2, message = "First name must not be less than 2 characters")
     private String firstName;
 
     @NotNull(message = "Last Name cannot be null")
-    @Size(min = 2, message = "Last name must not be less than 2 characters")
+    @Size(min = 2, message = "First name must not be less than 2 characters")
     private String lastName;
-
-    @Email
-    @NotNull(message = "Email cannot be null")
-    private String email;
-
-    @NotNull(message = "Password cannot be null")
-    @Size(min = 8, max = 16, message = "Password must be equal or greater than 8 characters and less than 16 characters")
-    private String password;
-
 }
